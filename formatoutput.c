a28a6f92538c58b266eac8842fcfee11c009bcab
lucykim-ConnectGaea <connectgaea@gmail.com>
2023-06-27 10:00:04 -0700

    minor fix

--- formatoutput.c
+++ formatoutput.c
@@ -1,53 +1,53 @@
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
 */
 
 int printf(const char *format, ...)
 {
-    var_list argmts;/**va_list that stores function arg*/
-    va_start(argmts, format);/**init argmts*/
-    int chnum;/**number of characters in the function*/
-    chnum = 0;
-    char chrt;/**stores a single character value*/
-    
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;/**advance after '%'*/
-            switch (*format)
-            {
-                case 'c':
-                    chrt = (char)va_arg(argmts, int);
-                    putchar(chrt);
-                    chnum++;
-                    break;
-                    
-                    case 's':
-                        char *string = va_arg(argmts, char *);
-                        while (*string)
-                            {
-                                putchar(*string);
-                            }
-                            string++;
-                            chnum++;
-                            break;
-                            
-                            case '%':
-                                putchar('%');
-                                chnum++;
-                                break;
-            }
-            else
-            {
-                putchar(*format);
-                chnum++;
-            }
-            format++;
-        }
-        va_end(args);
-        return (chnum);
-    }
-}   
+	int chnum;/**stores a single character value*/
+	char chrt;
+	char *str;
+
+	va_list argmts;/**va_list that stores function arg*/
+	va_start(argmts, format);/**init argmts*/
+	
+	chnum= 0;/**number of characters in the function*/
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;/**advance after '%'*/
+			switch (*format)
+			{
+				case 'c':
+					chrt = (char)va_arg(argmts, int);
+					putchar(chrt);
+					chnum++;
+					break;
+				case 's':
+					str = va_arg(argmts, char *);
+					while (*str)
+					{
+						putchar(*str);
+					}
+					str++;
+					chnum++;
+					break;
+				case '%':
+					putchar('%');
+					chnum++;
+					break;
+			}
+		}
+		else
+			{
+				putchar(*format);
+				chnum++;
+			}
+			format++;
+		}
+		va_end(argmts);
+		return(chnum);
 }
